c123d63dd20234342f1be263388d3c18ba15c49a
tejas.ghante <tejas.ghante@LT035-1>
2024-08-28 17:30:04 +0530

    token

---
+++ TicketTypeWebAPI/SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿CREATE DATABASE TicketPriorityDB
+USE TicketPriorityDB
+CREATE TABLE TicketPriority (
+PriorityId INT PRIMARY KEY,
+PriorityName VARCHAR(20)  NOT NULL,
+RespondWithin INT  NOT NULL,
+ResolveWithin INT  NOT NULL
+);
