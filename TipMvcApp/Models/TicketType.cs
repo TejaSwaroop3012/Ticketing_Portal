ba74c29ee37ba41c23f9afd7d809eda6a3c2fe21
tejas.ghante <tejas.ghante@LT035-1>
2024-08-30 15:13:06 +0530

    Validation

--- TipMvcApp/Models/TicketType.cs
+++ TipMvcApp/Models/TicketType.cs
@@ -4,9 +4,12 @@ namespace TipMvcApp.Models
 {
     public class TicketType
     {
+        [Required(ErrorMessage = "This field is necessary")]
         [Display(Name = "Ticket Type Id")]
         public int TicketTypeId { get; set; }
 
+        [Required(ErrorMessage = "This field is necessary")]
+        [MaxLength(30, ErrorMessage = "Ticket Type Name cannot be more than 30 characters")]
         [Display(Name = "Ticket Type Name")]
         public string? TicketTypeName { get; set; }
 
